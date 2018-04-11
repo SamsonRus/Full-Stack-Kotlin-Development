9ab2d338f140368c58cca6823808f58ca187c77b
Samsonov <Satan23666@mail.ru>
2018-04-11 10:33:44 +0300

    Lesson 2

---
+++ backend/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.apache.tools.ant.taskdefs.Jar
+import org.gradle.api.file.CopySpec
+import org.gradle.kotlin.dsl.compile
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+}
+val project = mapOf(name to "backend")
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Main-Class"] = "com.packtpub.HelloWorldKt"
+    }
+    from(configurations.runtime.map {
+        if (it.isDirectory) it else zipTree(it)
+    })
+    with(tasks["jar"] as CopySpec)
+}
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+allprojects {
+    group = "com.packtpub"
+    version = "1.0"
+    repositories {
+        jcenter()
+    }
+}
+
+plugins {
+    base
+}
+dependencies {
+    subprojects.forEach {
+        archives(it)
+    }
+}
