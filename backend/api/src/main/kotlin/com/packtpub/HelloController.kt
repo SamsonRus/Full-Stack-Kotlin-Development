60267f85acdd7eb2768451bce335b25541159b69
Samsonov <Satan23666@mail.ru>
2018-04-12 12:13:54 +0300

    Chapter 2.2

--- backend/api/src/main/kotlin/com/packtpub/HelloController.kt
+++ backend/api/src/main/kotlin/com/packtpub/HelloController.kt
@@ -1,11 +1,46 @@
 package com.packtpub
 
+import kotlinx.html.*
+import kotlinx.html.stream.createHTML
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController(private val helloSayer: HelloSayer) {
-    @GetMapping
-    fun sayHello(@RequestParam("name") name: String): String = helloSayer.sayHello(name)
+
+    val links = mapOf(
+            "Kotlin" to "https://github.com/JetBrains/kotlin",
+            "Full Stack Development" to "https://github.com/SamsonRus/Full-Stack-Kotlin-Development/"
+                     )
+
+    @GetMapping(produces = arrayOf("text/html"))
+    fun sayHello(@RequestParam(value = "name", required = false) name: String?)
+            : String =
+            createHTML(true).html {
+                head {
+                    title = "Full Stack Kotlin"
+                }
+                body {
+                    h4 {
+                        +helloSayer.sayHello(name ?: "User")
+                    }
+                    p {
+                        +"Welcome to full stack Kotlin"
+                    }
+                    p {
+                        +"Our Resources: "
+                        ul {
+                            links.map { (name, url) ->
+                                li {
+                                    a(url) {
+                                        target = ATarget.blank
+                                        +name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
 }
