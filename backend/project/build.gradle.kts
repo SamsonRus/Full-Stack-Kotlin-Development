e4e17d64a865129b3f267bf8b86c0d660bc612aa
Samsonov <Satan23666@mail.ru>
2018-04-13 14:29:04 +0300

    Chapter 4.1

--- backend/project/build.gradle.kts
+++ backend/project/build.gradle.kts
@@ -4,5 +4,8 @@ plugins {
 
 dependencies {
     val springBootVersion: String = parent.properties["springBootVersion"] as String
+    val mySQLConnectorVersion: String = parent.properties["mySQLConnectorVersion"] as String
     compile("org.springframework.boot:spring-boot-starter:$springBootVersion")
+    compile("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
+    compile("mysql:mysql-connector-java:$mySQLConnectorVersion")
 }
