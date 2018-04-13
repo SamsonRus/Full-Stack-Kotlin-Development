283e18cb3749548a5ff316ea53d4829605c5a1e8
Samsonov <Satan23666@mail.ru>
2018-04-13 15:37:24 +0300

    Chapter 4.2

--- backend/project/src/main/kotlin/com/packtpub/Model.kt
+++ backend/project/src/main/kotlin/com/packtpub/Model.kt
@@ -1,12 +1,16 @@
 package com.packtpub
 
+import javax.persistence.*
+
+@Entity
+@Table(name = "project")
 data class Project(
         val name: String,
         val url: String,
         val owner: String,
-        val language: Language,
-        val description: String? = null,
-        val tags: List<String> = listOf(),
+        val language: Language, @Id @GeneratedValue(strategy = GenerationType.AUTO)
+        val id: Long? = null,
+        val description: String? = null, @ElementCollection val tags: List<String> = listOf(),
         val license: String? = null)
 
 enum class Language {
